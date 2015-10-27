f06a02c38517dad7aa32f55ea0a10aed97f5a553
alt04 <tanikawa04@gmail.com>
2015-10-27 22:13:10 +0900

    Implement constructor of class Chord

---
+++ src/chord.js
@@ -0,0 +1,8 @@
+export default class Chord {
+    constructor({root, type, tensions, bass}) {
+        this.root = root;
+        this.type = type;
+        this.tensions = tensions;
+        this.bass = bass;
+    }
+}
