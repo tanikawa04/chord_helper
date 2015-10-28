0d9b9674662df62973a4ece71a131f4e507b0018
alt04 <tanikawa04@gmail.com>
2015-10-28 21:50:26 +0900

    Fix gulpfile and webpack config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,14 +7,14 @@ import plumber from 'gulp-plumber';
 import webpackConfig from './webpack.config';
 
 gulp.task('webpack', () => {
-	gulp.src('./src/*.js')
-		.pipe(plumber())
-		.pipe(webpack(webpackConfig))
-		.pipe(gulp.dest('./dist/'));
+    gulp.src('./src/*.js')
+        .pipe(plumber())
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', () => {
-	gulp.watch('./src/*.js', ['webpack']);
+    gulp.watch('./src/*.js', ['webpack']);
 });
 
 gulp.task('default', ['webpack']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-	entry: './src/chord_helper.js',
-	output: {
-		path: __dirname + '/',
-		filename: 'chord_helper.js'
-	},
-	devtool: '#source-map',
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader'
-			}
-		]
-	}
+    entry: './src/chord_helper.js',
+    output: {
+        path: __dirname + '/',
+        filename: 'chord_helper.js'
+    },
+    devtool: '#source-map',
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js']
+    }
 };
