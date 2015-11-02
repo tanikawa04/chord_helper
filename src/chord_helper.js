f33fb229481a70be961d2e8e18397ed7ab026aed
alt04 <tanikawa04@gmail.com>
2015-11-02 14:14:27 +0900

    Implement class ChordHelper

---
+++ src/chord_helper.js
@@ -0,0 +1,32 @@
+import Chord from './chord';
+import PitchClass from './pitch_class';
+import ChordType from './chord_type';
+import parseChord from './parse_chord';
+
+export default class ChordHelper {
+    static parseChord(str) {
+        return new _Chord(parseChord(str));
+    }
+
+    static setConfig(config) {
+        Chord.setConfig(config);
+    }
+}
+
+class _Chord {
+    constructor(obj) {
+        this._obj = obj;
+    }
+
+    toObj() {
+        return this._obj;
+    }
+
+    toNotes() {
+        const obj = this._obj;
+        const root = PitchClass[obj.root.replace('#', 'S').replace('b', 'F')];
+        const type = ChordType[obj.type];
+        const bass = (obj.bass === '') ? root : PitchClass[obj.bass.replace('#', 'S').replace('b', 'F')];
+        return new Chord(root, type, bass).getNotes();
+    }
+}
